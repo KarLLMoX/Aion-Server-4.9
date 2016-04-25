79730c255a7295a849ee8a47d6943a63678755ec
frozenkiller <frozenkiller@a70f7278-c47d-401d-a0e4-c9401b7f63ed>
2016-04-25 14:42:21 +0000

    PS back to default :D

--- AL-Tools/PacketSamurai_4.9/dist/start.bat
+++ AL-Tools/PacketSamurai_4.9/dist/start.bat
@@ -1,3 +1,3 @@
 @echo off
-start C:\java\jre1.8\bin\javaw -Xms8192m -Xmx8192m -cp ./libs/*;packetsamurai.jar com.aionemu.packetsamurai.PacketSamurai
+start javaw -Xms1024m -Xmx1024m -cp ./libs/*;packetsamurai.jar com.aionemu.packetsamurai.PacketSamurai
 exit
