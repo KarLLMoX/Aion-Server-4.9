0c7aff37af177aff7820158ed655b23e81673414
frozenkiller <frozenkiller@a70f7278-c47d-401d-a0e4-c9401b7f63ed>
2016-05-01 22:34:38 +0000

    Cleanup for Quest 14152

--- trunk/AL-Game/data/scripts/system/handlers/quest/heiron/_14152SpoilerAlert.java
+++ trunk/AL-Game/data/scripts/system/handlers/quest/heiron/_14152SpoilerAlert.java
@@ -18,14 +18,12 @@ package quest.heiron;
 
 import com.aionemu.gameserver.model.gameobjects.Npc;
 import com.aionemu.gameserver.model.gameobjects.player.Player;
-import com.aionemu.gameserver.network.aion.serverpackets.SM_DIALOG_WINDOW;
 import com.aionemu.gameserver.questEngine.handlers.QuestHandler;
 import com.aionemu.gameserver.model.DialogAction;
 import com.aionemu.gameserver.questEngine.model.QuestEnv;
 import com.aionemu.gameserver.questEngine.model.QuestState;
 import com.aionemu.gameserver.questEngine.model.QuestStatus;
 import com.aionemu.gameserver.services.QuestService;
-import com.aionemu.gameserver.utils.PacketSendUtility;
 
 /**
  * @author FrozenKiller
@@ -60,7 +58,6 @@ import com.aionemu.gameserver.utils.PacketSendUtility;
         DialogAction dialog = env.getDialog();
 		
 		if (qs == null || qs.getStatus() == QuestStatus.NONE) {
-			int var = qs.getQuestVarById(0);
             if (targetId == 204504) {
 				switch (dialog) {
 					case QUEST_SELECT: {
