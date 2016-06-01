15b44faab7f58ce1a62634299f6a14c100d478d9
falke34 <falke34@a70f7278-c47d-401d-a0e4-c9401b7f63ed>
2016-06-01 07:00:36 +0000

    update Event: Wishing Fountain, apply Siege Occupy Count Patch

--- trunk/AL-Game/data/scripts/system/database/mysql5/MySQL5SiegeDAO.java
+++ trunk/AL-Game/data/scripts/system/database/mysql5/MySQL5SiegeDAO.java
@@ -37,9 +37,9 @@ import java.util.Map;
 public class MySQL5SiegeDAO extends SiegeDAO {
 
     private static final Logger log = LoggerFactory.getLogger(MySQL5SiegeDAO.class);
-    public static final String SELECT_QUERY = "SELECT `id`, `race`, `legion_id` FROM `siege_locations`";
-    public static final String INSERT_QUERY = "INSERT INTO `siege_locations` (`id`, `race`, `legion_id`) VALUES(?, ?, ?)";
-    public static final String UPDATE_QUERY = "UPDATE `siege_locations` SET  `race` = ?, `legion_id` = ? WHERE `id` = ?";
+    public static final String SELECT_QUERY = "SELECT `id`, `race`, `legion_id`, `occupy_count` FROM `siege_locations`";
+    public static final String INSERT_QUERY = "INSERT INTO `siege_locations` (`id`, `race`, `legion_id`, `occupy_count`) VALUES(?, ?, ?, ?)";
+    public static final String UPDATE_QUERY = "UPDATE `siege_locations` SET  `race` = ?, `legion_id` = ?, `occupy_count` = ? WHERE `id` = ?";
 
     @Override
     public boolean loadSiegeLocations(final Map<Integer, SiegeLocation> locations) {
@@ -56,6 +56,7 @@ public class MySQL5SiegeDAO extends SiegeDAO {
                 SiegeLocation loc = locations.get(resultSet.getInt("id"));
                 loc.setRace(SiegeRace.valueOf(resultSet.getString("race")));
                 loc.setLegionId(resultSet.getInt("legion_id"));
+                loc.setOccupyCount(resultSet.getInt("occupy_count"));
                 loaded.add(loc.getLocationId());
             }
             resultSet.close();
@@ -86,10 +87,11 @@ public class MySQL5SiegeDAO extends SiegeDAO {
             stmt.setString(1, siegeLocation.getRace().toString());
             stmt.setInt(2, siegeLocation.getLegionId());
             stmt.setInt(3, siegeLocation.getLocationId());
+            stmt.setInt(4, siegeLocation.getOccupyCount());
             stmt.execute();
         } catch (Exception e) {
             log.error("Error update Siege Location: " + siegeLocation.getLocationId() + " to race: "
-                    + siegeLocation.getRace().toString(), e);
+                    + siegeLocation.getRace().toString(), e + "Occupy_Count: " + siegeLocation.getOccupyCount() );
             return false;
         } finally {
             DatabaseFactory.close(stmt, con);
@@ -110,6 +112,7 @@ public class MySQL5SiegeDAO extends SiegeDAO {
             stmt.setInt(1, siegeLocation.getLocationId());
             stmt.setString(2, siegeLocation.getRace().toString());
             stmt.setInt(3, siegeLocation.getLegionId());
+            stmt.setInt(4, siegeLocation.getOccupyCount());
             stmt.execute();
         } catch (Exception e) {
             log.error("Error insert Siege Location: " + siegeLocation.getLocationId(), e);
