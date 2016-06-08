8190e48324eda5410ce72a051ae3704e48f4dce3
frozenkiller <frozenkiller@a70f7278-c47d-401d-a0e4-c9401b7f63ed>
2016-06-08 01:30:57 +0000

    Fixed CharlesrunerkAI.java (Now you can Talk and use this NPC) + Added Spawn to Cygnea....

--- trunk/AL-Game/data/scripts/system/handlers/ai/worlds/cygnea/CharlesrunerkAI.java
+++ trunk/AL-Game/data/scripts/system/handlers/ai/worlds/cygnea/CharlesrunerkAI.java
@@ -9,6 +9,17 @@
  *  Aion-Lightning is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details. */**
+ * This file is part of Aion-Lightning <aion-lightning.org>.
+ *
+ *  Aion-Lightning is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Aion-Lightning is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details. *
  *  You should have received a copy of the GNU General Public License
  *  along with Aion-Lightning.
@@ -17,20 +28,16 @@
 package ai.worlds.cygnea;
 
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Collection;
 
-import com.aionemu.commons.database.dao.DAOManager;
 import com.aionemu.commons.network.util.ThreadPoolManager;
 import com.aionemu.commons.utils.Rnd;
 import com.aionemu.gameserver.GameServer;
 import com.aionemu.gameserver.ai2.AIName;
 import com.aionemu.gameserver.ai2.NpcAI2;
-import com.aionemu.gameserver.dao.PlayerWorldBanDAO;
+import com.aionemu.gameserver.ai2.handler.TalkEventHandler;
 import com.aionemu.gameserver.model.gameobjects.Npc;
 import com.aionemu.gameserver.model.gameobjects.player.Player;
-import com.aionemu.gameserver.utils.PacketSendUtility;
-import com.aionemu.gameserver.world.World;
 import com.aionemu.gameserver.world.WorldPosition;
 
 /**
@@ -43,7 +50,6 @@ public class CharlesrunerkAI extends NpcAI2 {
     // NPC Charlesrunerk hat 7 Random Spawn Positionen,
     // an jeder dieser Positionen bleibt er für 1 Stunde
 	
-	WorldPosition actualPos = new WorldPosition(getOwner().getWorldId());
 	Collection<WorldPosition> loc = new ArrayList<WorldPosition>();
 	Npc npc = null;
 	
@@ -52,16 +58,16 @@ public class CharlesrunerkAI extends NpcAI2 {
 	{
 		super.handleSpawned();
         npc = getOwner();
-        int time = 5; //respawnTime in minutes -- default 60 min.
+        int time = 60; //respawnTime in minutes -- default 60 min.
         
         loc.clear();
-        loc.add(new WorldPosition(npc.getWorldId(), 480.80933f, 1918.559f, 466.5252f, (byte) 15));
-        loc.add(new WorldPosition(npc.getWorldId(), 1120.8098f, 2921.0552f, 291.25f, (byte) 90));
-        loc.add(new WorldPosition(npc.getWorldId(), 1181.7f, 1558.1765f, 465.75f, (byte) 105));
-        loc.add(new WorldPosition(npc.getWorldId(), 1398.9495f, 639.26959f, 581.25f, (byte) 44));
-        loc.add(new WorldPosition(npc.getWorldId(), 2092.6746f, 2840.4351f, 322.962f, (byte) 15));
-        loc.add(new WorldPosition(npc.getWorldId(), 2400.4988f, 1524.213f, 439.5217f, (byte) 60));
-        loc.add(new WorldPosition(npc.getWorldId(), 2826.0615f, 766.99139f, 564.83984f, (byte) 15));
+        loc.add(new WorldPosition(210070000, 480.80933f, 1918.559f, 466.5252f, (byte) 15));
+        loc.add(new WorldPosition(210070000, 1120.8098f, 2921.0552f, 291.25f, (byte) 90));
+        loc.add(new WorldPosition(210070000, 1181.7f, 1558.1765f, 465.75f, (byte) 105));
+        loc.add(new WorldPosition(210070000, 1398.9495f, 639.26959f, 581.25f, (byte) 44));
+        loc.add(new WorldPosition(210070000, 2092.6746f, 2840.4351f, 322.962f, (byte) 15));
+        loc.add(new WorldPosition(210070000, 2400.4988f, 1524.213f, 439.5217f, (byte) 60));
+        loc.add(new WorldPosition(210070000, 2826.0615f, 766.99139f, 564.83984f, (byte) 15));
 
         ThreadPoolManager.getInstance().schedule(new Runnable() 
         {
@@ -76,4 +82,87 @@ public class CharlesrunerkAI extends NpcAI2 {
             }
         }, 1000 * 60 * time); 
 	}
+	
+	@Override
+    protected void handleDialogStart(Player player) {
+        TalkEventHandler.onTalk(this, player);
+    }
+
+    @Override
+    protected void handleDialogFinish(Player creature) {
+        TalkEventHandler.onFinishTalk(this, creature);
+    }
+}
+
+ *  You should have received a copy of the GNU General Public License
+ *  along with Aion-Lightning.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ */
+package ai.worlds.cygnea;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import com.aionemu.commons.network.util.ThreadPoolManager;
+import com.aionemu.commons.utils.Rnd;
+import com.aionemu.gameserver.GameServer;
+import com.aionemu.gameserver.ai2.AIName;
+import com.aionemu.gameserver.ai2.NpcAI2;
+import com.aionemu.gameserver.ai2.handler.TalkEventHandler;
+import com.aionemu.gameserver.model.gameobjects.Npc;
+import com.aionemu.gameserver.model.gameobjects.player.Player;
+import com.aionemu.gameserver.world.WorldPosition;
+
+/**
+ * @author Falke_34, CoolyT
+ */
+@AIName("charlesrunerk")
+public class CharlesrunerkAI extends NpcAI2 {
+
+	//TODO
+    // NPC Charlesrunerk hat 7 Random Spawn Positionen,
+    // an jeder dieser Positionen bleibt er für 1 Stunde
+	
+	Collection<WorldPosition> loc = new ArrayList<WorldPosition>();
+	Npc npc = null;
+	
+	@Override
+	protected void handleSpawned() 
+	{
+		super.handleSpawned();
+        npc = getOwner();
+        int time = 1; //respawnTime in minutes -- default 60 min.
+        
+        loc.clear();
+        loc.add(new WorldPosition(210070000, 480.80933f, 1918.559f, 466.5252f, (byte) 15));
+        loc.add(new WorldPosition(210070000, 1120.8098f, 2921.0552f, 291.25f, (byte) 90));
+        loc.add(new WorldPosition(210070000, 1181.7f, 1558.1765f, 465.75f, (byte) 105));
+        loc.add(new WorldPosition(210070000, 1398.9495f, 639.26959f, 581.25f, (byte) 44));
+        loc.add(new WorldPosition(210070000, 2092.6746f, 2840.4351f, 322.962f, (byte) 15));
+        loc.add(new WorldPosition(210070000, 2400.4988f, 1524.213f, 439.5217f, (byte) 60));
+        loc.add(new WorldPosition(210070000, 2826.0615f, 766.99139f, 564.83984f, (byte) 15));
+
+        ThreadPoolManager.getInstance().schedule(new Runnable() 
+        {
+            @Override
+            public void run() 
+            {
+            	int index = Rnd.get(0,6);
+            	WorldPosition random = (WorldPosition) loc.toArray()[index];
+            	spawn(805338, random.getX(), random.getY(), random.getZ(), random.getHeading());
+            	npc.getController().delete();
+            	GameServer.log.info("CharlesrunerkAI: Spawned on Pos : "+ random.toString());
+            }
+        }, 1000 * 60 * time); 
+	}
+	
+	@Override
+    protected void handleDialogStart(Player player) {
+        TalkEventHandler.onTalk(this, player);
+    }
+
+    @Override
+    protected void handleDialogFinish(Player creature) {
+        TalkEventHandler.onFinishTalk(this, creature);
+    }
 }
