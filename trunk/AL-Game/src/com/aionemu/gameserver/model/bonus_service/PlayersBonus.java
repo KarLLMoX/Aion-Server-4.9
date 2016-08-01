70b4c67f172e447ce6aa590fa32b527efc417733
falke34 <falke34@a70f7278-c47d-401d-a0e4-c9401b7f63ed>
2016-08-01 09:52:48 +0000

    some small Fixes/Updates

--- trunk/AL-Game/src/com/aionemu/gameserver/model/bonus_service/PlayersBonus.java
+++ trunk/AL-Game/src/com/aionemu/gameserver/model/bonus_service/PlayersBonus.java
@@ -1,3 +1,19 @@
+/**
+ * This file is part of Aion-Lightning <aion-lightning.org>.
+ *
+ *  Aion-Lightning is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Aion-Lightning is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details. *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Aion-Lightning.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.aionemu.gameserver.model.bonus_service;
 
 import com.aionemu.gameserver.dataholders.DataManager;
@@ -19,7 +35,6 @@ import java.util.List;
 /**
  * Created by Ace on 31/07/2016.
  */
-
 public class PlayersBonus implements StatOwner
 {
     private static final Logger log = LoggerFactory.getLogger(PlayersBonus.class);
@@ -49,4 +64,3 @@ public class PlayersBonus implements StatOwner
         player.getGameStats().endEffect(this);
     }
 }
-
